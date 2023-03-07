42b405a8db588825508c3921383ddbc02e08df68
Jeancarlos Alban <verfrut.app.2020@gmail.com>
2023-03-06 21:15:51 -0500

    asd

--- app/src/main/java/com/devsoft/segser/MainActivity.java
+++ app/src/main/java/com/devsoft/segser/MainActivity.java
@@ -1,25 +1,61 @@
 package com.devsoft.segser;
 
-import android.os.Bundle;
-import android.widget.Button;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
+import android.app.DownloadManager;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.auth0.android.jwt.JWT;
+import com.basusingh.beautifulprogressdialog.BeautifulProgressDialog;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+import cz.msebera.android.httpclient.Header;
 import id.ionbit.ionalert.IonAlert;
 
 
 public class MainActivity extends AppCompatActivity {
     TextInputEditText txtusuario,txtclave;
     Button btningresar;
-//    AsyncHttpClient client;
-    //RequestParams params;
 
-
-
+    RequestQueue request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +64,35 @@ public class MainActivity extends AppCompatActivity {
         txtusuario=findViewById(R.id.txtusuario);
         txtclave=findViewById(R.id.txtclave);
         btningresar=findViewById(R.id.btningresar);
-     //  client=new AsyncHttpClient();
-      //  params=new RequestParams();
+
+        request=Volley.newRequestQueue(MainActivity.this);
+
+        if(validaPermisos()){
+            btningresar.setEnabled(true);
+        }else{
+            btningresar.setEnabled(false);
+        }
+
+        SharedPreferences preferences=getSharedPreferences("SEGSER",MODE_PRIVATE);
+        String token= preferences.getString("token","Invitado");
+        if(!token.equals("Invitado")){
+            JWT jwt=new JWT(token);
+            boolean isExpired=jwt.isExpired(10);
+            if(!isExpired){
+                Intent midintent = new Intent(MainActivity.this, MenuPrincipalActivity.class);
+                Bundle mibundle = new Bundle();
+
+
+                mibundle.putString("token",token);
+
+
+                midintent.putExtras(mibundle);
+                startActivity(midintent);
+                super.finish();
+            }
+        }
+
+
 
         btningresar.setOnClickListener(view -> {
             if(txtusuario.getText().toString().isEmpty()||txtclave.getText().toString().isEmpty()){
@@ -47,7 +110,164 @@ public class MainActivity extends AppCompatActivity {
 
     private void login(String usuario, String clave) {
 
+
+
+        BeautifulProgressDialog progressDialog = new BeautifulProgressDialog(MainActivity.this,
+                BeautifulProgressDialog.withImage,
+                "Espere porfavor");
+        progressDialog.show();
         String url=Global.url+"login";
+        StringRequest jsonObjectRequest=new StringRequest(
+                Request.Method.POST, url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+
+                respuesta(response);
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                new IonAlert(MainActivity.this,IonAlert.WARNING_TYPE)
+                        .setTitleText("SEGSER SEGURIDAD")
+                        .setContentText("ERROR EN SERVICIO DE LOGIN \n"+error.getMessage())
+                        .show();
+
+
+            }
+        }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros=new HashMap<>();
+                parametros.put("USERNAME",usuario);
+                parametros.put("PASSWORD",clave);
+                return parametros;
+            }
+         /*   @Override
+            public Map<String, String>getHeaders() throws AuthFailureError{
+                HashMap headers = new HashMap();
+                headers.put("Content-Type","application/x-www-form-urlencoded");
+                return headers;
+            }*/
+
+        };
+
+
+        request.add(jsonObjectRequest);
+
 
     }
+
+
+    private void respuesta(String response) {
+
+        try {
+            JSONObject json=new JSONObject(response);
+            JSONObject token=json.getJSONObject("token");
+            if(token.getString("status").equals("ok")){
+                String tokens=token.getString("token");
+                SharedPreferences preferences=getSharedPreferences("SEGSER",MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("token", tokens);
+                editor.commit();
+                Intent midintent = new Intent(MainActivity.this, MenuPrincipalActivity.class);
+                Bundle mibundle = new Bundle();
+
+
+                mibundle.putString("token",tokens);
+
+
+                midintent.putExtras(mibundle);
+                startActivity(midintent);
+                super.finish();
+            }else{
+                new IonAlert(MainActivity.this,IonAlert.WARNING_TYPE)
+                        .setTitleText("SEGSER SEGURIDAD")
+                        .setContentText("USUARIO O CONTRASEÑA INCORRECTOS")
+                        .show();
+            }
+
+
+
+
+        }catch (Exception e){
+            System.out.println("error "+e.getMessage());
+        }
+
+    }
+
+    private boolean validaPermisos() {
+
+        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
+            return true;
+        }
+
+        if((checkSelfPermission(CAMERA)== PackageManager.PERMISSION_GRANTED)&&
+                (checkSelfPermission(WRITE_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED)&& (checkSelfPermission(ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED)){
+            return true;
+        }
+
+        if((shouldShowRequestPermissionRationale(CAMERA)) ||
+                (shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE))||(shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION))){
+            cargarDialogoRecomendacion();
+        }else{
+            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE,CAMERA,ACCESS_FINE_LOCATION},100);
+        }
+
+        return false;
+    }
+    private void cargarDialogoRecomendacion() {
+        AlertDialog.Builder dialogo=new AlertDialog.Builder(MainActivity.this);
+        dialogo.setTitle("Permisos Desactivados");
+        dialogo.setMessage("Debe aceptar los permisos para el correcto funcionamiento de la App");
+
+        dialogo.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE,CAMERA,ACCESS_FINE_LOCATION},100);
+            }
+        });
+        dialogo.show();
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode==100){
+            if(grantResults.length==3 && grantResults[0]==PackageManager.PERMISSION_GRANTED
+                    && grantResults[1]==PackageManager.PERMISSION_GRANTED && grantResults[2]==PackageManager.PERMISSION_GRANTED){
+                btningresar.setEnabled(true);
+            }else{
+                solicitarPermisosManual();
+            }
+        }
+
+    }
+    private void solicitarPermisosManual() {
+        final CharSequence[] opciones={"si","no"};
+        final AlertDialog.Builder alertOpciones=new AlertDialog.Builder(MainActivity.this);
+        alertOpciones.setTitle("¿Desea configurar los permisos de forma manual?");
+        alertOpciones.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (opciones[i].equals("si")){
+                    Intent intent=new Intent();
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri=Uri.fromParts("package",getPackageName(),null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(),"Los permisos no fueron aceptados", Toast.LENGTH_SHORT).show();
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        alertOpciones.show();
+    }
+
 }
